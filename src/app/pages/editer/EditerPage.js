5826215a2fb71868fc0535bb5332efb22b98a409
rijvan <rijvan.mohammad@indovations.in>
2022-09-30 11:25:43 +0530

    grapesediter implemented

--- src/app/pages/editer/EditerPage.js
+++ src/app/pages/editer/EditerPage.js
@@ -2,13 +2,14 @@ import React, {useEffect} from 'react'
 import {useIntl} from 'react-intl'
 import {PageTitle} from '../../../_metronic/layout/core'
 import { Editer } from './components/EditerUi'
+import { GrapesEditer } from './components/GrapesEditer'
 
 const EditerPage = () => {
   const intl = useIntl()
   return (
     <>
       <PageTitle breadcrumbs={[]}>Editer</PageTitle>
-      <Editer />
+      <GrapesEditer />
     </>
   )
 }
---
+++ src/app/pages/editer/components/GrapesEditer.js
@@ -0,0 +1,67 @@
+import React, {useEffect, useRef} from 'react'
+import grapesjs from 'grapesjs'
+import 'grapesjs/dist/css/grapes.min.css'
+import 'grapesjs/dist/grapes.min.js'
+import 'grapesjs-preset-webpage/dist/index'
+
+const GrapesEditer = () => {
+
+    useEffect(() => {
+    grapesjs.init({
+        container: '#gjs',
+        height: '700px',
+        width: '100%',
+        plugins: ['gjs-preset-webpage'],
+        storageManager: {
+        id: 'gjs-',
+        type: 'local',
+        autosave: true,
+        storeComponents: true,
+        storeStyles: true,
+        storeHtml: true,
+        storeCss: true,
+        },
+        deviceManager: {
+        devices:
+        [
+            {
+            id: 'desktop',
+            name: 'Desktop',
+            width: '',
+            },
+            {
+            id: 'tablet',
+            name: 'Tablet',
+            width: '768px',
+            widthMedia: '992px',
+            },
+            {
+            id: 'mobilePortrait',
+            name: 'Mobile portrait',
+            width: '320px',
+            widthMedia: '575px',
+            },
+        ]
+        },
+        pluginsOpts: {
+        'grapesjs-preset-webpage': {
+            blocksBasicOpts: {
+            blocks: ['column1', 'column2', 'column3', 'column3-7', 'text',     'link', 'image', 'video'],
+            flexGrid: 1,
+            },
+            blocks: ['link-block', 'quote', 'text-basic'],
+        },
+        }
+    })
+    },[]);
+     
+  return (
+    <>
+    <div>
+    <div id="gjs"></div>
+    </div>
+    </>
+  )
+}
+
+export {GrapesEditer}
