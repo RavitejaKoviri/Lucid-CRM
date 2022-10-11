5bb17a1f448f1ca125e8069112864f0f8c10da1c
janibegum <mabegum13@gmail.com>
2022-10-11 15:06:07 +0530

    leads

--- src/app/modules/leads/leads-list/components/header/LeadsListToolbar.tsx
+++ src/app/modules/leads/leads-list/components/header/LeadsListToolbar.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom'
 import { KTSVG } from '../../../../../../_metronic/helpers'
 import { useListView } from '../../core/ListViewProvider'
 import { LeadsListFilter } from './LeadsListFilter'
@@ -6,7 +7,9 @@ const LeadsListToolbar = () => {
   const { setItemIdForUpdate } = useListView()
   const openAddUserModal = () => {
     setItemIdForUpdate(null)
-  }
+  } 
+   const navigation=useNavigate()
+
 
   return (
     <div className='d-flex justify-content-end' data-kt-user-table-toolbar='base'>
@@ -20,7 +23,9 @@ const LeadsListToolbar = () => {
       {/* end::Export */}
 
       {/* begin::Add user */}
-      <button type='button' className='btn btn-primary' onClick={openAddUserModal}>
+      <button type='button' className='btn btn-primary' onClick={()=>{
+        navigation('leadadduser')
+      }}>
         <KTSVG path='/media/icons/duotune/arrows/arr075.svg' className='svg-icon-2' />
         Add User
       </button>
--- src/app/routing/PrivateRoutes.tsx
+++ src/app/routing/PrivateRoutes.tsx
@@ -10,6 +10,7 @@ import { getCSSVariableValue } from '../../_metronic/assets/ts/_utils'
 import { WithChildren } from '../../_metronic/helpers'
 import BuilderPageWrapper from '../pages/layout-builder/BuilderPageWrapper'
 import Adduser from '../modules/user-management/users-list/user-edit-modal/Adduser'
+import LeadAdduser from '../modules/leads/leads-list/components/header/LeadAdduser'
 
 
 
@@ -41,6 +42,8 @@ const PrivateRoutes = () => {
         {/* <Route path='targets' element={<TargetsWrapper />} /> */}
         <Route path='editer' element={<EditerPage />} />
         <Route path='apps/user-management/users/adduser' element={<Adduser />} />
+        <Route path='leads/list/leadadduser' element={<LeadAdduser />} />
+
         {/* Lazy Modules */}
         <Route
           path='crafted/pages/profile/*'
