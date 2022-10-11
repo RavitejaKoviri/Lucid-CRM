fbae39ccafd4e79dfeaed38b7f7f3d98f4a7a87e
Jayakrishna <Jayakrishna@PractiScs-MacBook-Air.local>
2022-10-11 12:33:46 +0530

    conflict resolved

---
+++ src/app/modules/Contact/ContactsPage.tsx
@@ -0,0 +1,39 @@
+import { Route, Routes, Outlet, Navigate } from 'react-router-dom'
+import { PageLink, PageTitle } from '../../../_metronic/layout/core'
+import { ContactsListWrapper } from './contacts-list/ContactsList'
+
+const contactsBreadcrumbs: Array<PageLink> = [
+  {
+    title: 'contacts',
+    path: 'contacts',
+    isSeparator: false,
+    isActive: false,
+  },
+  {
+    title: '',
+    path: '',
+    isSeparator: true,
+    isActive: false,
+  },
+]
+
+const ContactsPage = () => {
+  return (
+    <Routes>
+      <Route element={<Outlet />}>
+        <Route
+          path='contacts'
+          element={
+            <>
+              <PageTitle breadcrumbs={contactsBreadcrumbs}>Contacts</PageTitle>
+              <ContactsListWrapper />
+            </>
+          }
+        />
+      </Route>
+      <Route index element={<Navigate to='contacts' />} />
+    </Routes>
+  )
+}
+
+export default ContactsPage
