e4438c782d8686f152ad968f4cb7e5789c580259
Ashok Bathu <ab@websoc.co.in>
2022-10-12 11:14:11 +0000

    Revert "ticket status (pull request #12)"

--- src/app/modules/Tickets/Ticket-list/_redux/ticketAction.ts
+++ src/app/modules/Tickets/Ticket-list/_redux/ticketAction.ts
@@ -6,84 +6,13 @@ const { actions } = TicketSlice;
 
 
 //getAll user details
-export const getcompany = (token: any) => (dispatch: any) => {
-  dispatch(actions.startCall({ callType: callTypes.action }));
-  return requestFromServer
-    .getAllcompanies(token)
-    .then((response) => {
-      const { data } = response;
-      dispatch(actions.fetchedAllComapniesDetails({ data }));
-    })
-    .catch((error) => {
-      error.clientMessage = "Can't find patient test reports";;
-      dispatch(actions.catchError({ error, callType: callTypes.action }));
-    });
-};
-
-export const getAllUsers = (token: any) => (dispatch: any) => {
+export const getAllTickets = (token: any) => (dispatch: any) => {
   dispatch(actions.startCall({ callType: callTypes.action }));
   return requestFromServer
     .getAllUsers(token)
     .then((response) => {
       const { data } = response;
-      dispatch(actions.fetchedAllUsers({ data }));
-    })
-    .catch((error) => {
-      error.clientMessage = "Can't find patient test reports";;
-      dispatch(actions.catchError({ error, callType: callTypes.action }));
-    });
-};
-
-
-export const getAllcustomers = (token: any) => (dispatch: any) => {
-  dispatch(actions.startCall({ callType: callTypes.action }));
-  return requestFromServer
-    .getAllcustomers(token)
-    .then((response) => {
-      const { data } = response;
-      dispatch(actions.fetchedAllcustomers({ data }));
-    })
-    .catch((error) => {
-      error.clientMessage = "Can't find patient test reports";;
-      dispatch(actions.catchError({ error, callType: callTypes.action }));
-    });
-};
-
-
-export const CreateTicket = (data: any, token: any) => (dispatch: any) =>
-  requestFromServer
-    .CreateTicket(data, token)
-    .then((response) => {
-      const { data } = response;
-      dispatch(actions.fetchedTicket({ data }));
-    })
-    .catch((error) => {
-      // eslint-disable-next-line no-param-reassign
-      error.clientMessage = "Can't find";
-      dispatch(actions.catchError({ error, callType: callTypes.list }));
-    });
-
-export const getAllticketStatus = (token: any) => (dispatch: any) => {
-  dispatch(actions.startCall({ callType: callTypes.action }));
-  return requestFromServer
-    .getAllticketStatus(token)
-    .then((response) => {
-      const { data } = response;
-      dispatch(actions.fetchedAllticketStatus({ data }));
-    })
-    .catch((error) => {
-      error.clientMessage = "Can't find patient test reports";;
-      dispatch(actions.catchError({ error, callType: callTypes.action }));
-    });
-};
-
-export const getAllTickets = (token: any) => (dispatch: any) => {
-  dispatch(actions.startCall({ callType: callTypes.action }));
-  return requestFromServer
-    .getAllTickets(token)
-    .then((response) => {
-      const { data } = response;
-      dispatch(actions.fetchedAllTickets({ data }));
+      dispatch(actions.getedAllUsersDetails({ data }));
     })
     .catch((error) => {
       error.clientMessage = "Can't find patient test reports";;
