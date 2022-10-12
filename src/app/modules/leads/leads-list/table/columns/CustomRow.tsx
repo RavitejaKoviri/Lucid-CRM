4efa5b7b144447866038b55d581d4a8788d2afa2
pradeep <pradeepinfo478@gmail.com>
2022-10-12 19:29:20 +0530

    filter

--- src/app/modules/leads/leads-list/table/columns/CustomRow.tsx
+++ src/app/modules/leads/leads-list/table/columns/CustomRow.tsx
@@ -1,25 +1,42 @@
 import clsx from 'clsx'
-import { FC } from 'react'
+import { FC, useContext } from 'react'
 import { Row } from 'react-table'
 import { Lead } from '../../core/_models'
+import UserContext from './context'
 
 type Props = {
   row: Row<Lead>
 }
 
-const CustomRow: FC<Props> = ({ row }) => (
-  <tr {...row.getRowProps()}>
-    {row.cells.map((cell) => {
-      return (
-        <td
-          {...cell.getCellProps()}
-          className={clsx({ 'text-end min-w-100px': cell.column.id === 'actions' })}
-        >
-          {cell.render('Cell')}
-        </td>
-      )
-    })}
-  </tr>
-)
+
+
+const CustomRow: FC<Props> = ({ row }) => {
+  const { searchTerm } = useContext(UserContext);
+  return (
+
+    <tr {...row.getRowProps()}>
+      {row.cells.filter((val: any) => {
+        if (searchTerm === "") {
+          return val;
+        }
+        console.log(val, "val")
+        if (val?.value?.toLowerCase()?.includes(searchTerm?.toLowerCase())) {
+          console.log(val, "useC");
+          return val;
+        }
+      }).map((cell) => {
+        return (
+          <td
+            {...cell.getCellProps()}
+            className={clsx({ 'text-end min-w-100px': cell.column.id === 'actions' })}
+          >
+            {cell.render('Cell')}
+          </td>
+        )
+      })}
+    </tr>
+  )
+}
+
 
 export { CustomRow }
