1ea9a8ff9de224d25e20d714e330d211e1b874fc
gopi <nadh.gopi8@gmail.com>
2022-11-21 11:04:41 +0530

    account setting

--- src/app/pages/dashboard/_redux/dashboardAction.ts
+++ src/app/pages/dashboard/_redux/dashboardAction.ts
@@ -3,8 +3,6 @@ import { DashboardSlice, callTypes } from "./dashboardSlice";
 
 const { actions } = DashboardSlice;
 
-
-
 //getAll user details
 export const getAllBookings = () => (dispatch: any) => {
   dispatch(actions.startCall({ callType: callTypes.action }));
@@ -15,7 +13,7 @@ export const getAllBookings = () => (dispatch: any) => {
       dispatch(actions.fetchedAllBooking({ data }));
     })
     .catch((error) => {
-      error.clientMessage = "Can't find patient test reports";;
+      error.clientMessage = "Can't find patient test reports";
       dispatch(actions.catchError({ error, callType: callTypes.action }));
     });
 };
@@ -28,36 +26,37 @@ export const getAllTasks = (token: any, companyId: any) => (dispatch: any) => {
       dispatch(actions.fetchedAllTasks({ data }));
     })
     .catch((error) => {
-      error.clientMessage = "Can't find patient test reports";;
+      error.clientMessage = "Can't find patient test reports";
       dispatch(actions.catchError({ error, callType: callTypes.action }));
     });
 };
-export const getAllDeals = (token: any,companyId:any) => (dispatch: any) => {
+export const getAllDeals = (token: any, companyId: any) => (dispatch: any) => {
   dispatch(actions.startCall({ callType: callTypes.action }));
   return requestFromServer
-    .fetchAllDeals(token,companyId)
+    .fetchAllDeals(token, companyId)
     .then((response) => {
       const { data } = response;
       dispatch(actions.fetchedAllDeals({ data }));
     })
     .catch((error) => {
-      error.clientMessage = "Can't find patient test reports";;
+      error.clientMessage = "Can't find patient test reports";
       dispatch(actions.catchError({ error, callType: callTypes.action }));
     });
 };
-export const getAllContacts = (token: any,companyId:any) => (dispatch: any) => {
-  dispatch(actions.startCall({ callType: callTypes.action }));
-  return requestFromServer
-    .fetchAllContacts(token,companyId)
-    .then((response) => {
-      const { data } = response;
-      dispatch(actions.fetchedAllContacts({ data }));
-    })
-    .catch((error) => {
-      error.clientMessage = "Can't find patient test reports";;
-      dispatch(actions.catchError({ error, callType: callTypes.action }));
-    });
-};
+export const getAllContacts =
+  (token: any, companyId: any) => (dispatch: any) => {
+    dispatch(actions.startCall({ callType: callTypes.action }));
+    return requestFromServer
+      .fetchAllContacts(token, companyId)
+      .then((response) => {
+        const { data } = response;
+        dispatch(actions.fetchedAllContacts({ data }));
+      })
+      .catch((error) => {
+        error.clientMessage = "Can't find patient test reports";
+        dispatch(actions.catchError({ error, callType: callTypes.action }));
+      });
+  };
 export const getAllCampaigns = (token: any) => (dispatch: any) => {
   dispatch(actions.startCall({ callType: callTypes.action }));
   return requestFromServer
@@ -67,20 +66,20 @@ export const getAllCampaigns = (token: any) => (dispatch: any) => {
       dispatch(actions.fetchedAllCampaigns({ data }));
     })
     .catch((error) => {
-      error.clientMessage = "Can't find patient test reports";;
+      error.clientMessage = "Can't find patient test reports";
       dispatch(actions.catchError({ error, callType: callTypes.action }));
     });
 };
-export const getAllLeads = (token: any,companyId:any) => (dispatch: any) => {
+export const getAllLeads = (token: any, companyId: any) => (dispatch: any) => {
   dispatch(actions.startCall({ callType: callTypes.action }));
   return requestFromServer
-    .fetchAllLeads(token,companyId)
+    .fetchAllLeads(token, companyId)
     .then((response) => {
       const { data } = response;
       dispatch(actions.fetchedAllLeads({ data }));
     })
     .catch((error) => {
-      error.clientMessage = "Can't find patient test reports";;
+      error.clientMessage = "Can't find patient test reports";
       dispatch(actions.catchError({ error, callType: callTypes.action }));
     });
 };
@@ -94,34 +93,62 @@ export const getCdrs = (token: any) => (dispatch: any) => {
       dispatch(actions.fetchedCDRSDetails({ data }));
     })
     .catch((error) => {
-      error.clientMessage = "Can't find patient test reports";;
+      error.clientMessage = "Can't find patient test reports";
       dispatch(actions.catchError({ error, callType: callTypes.action }));
     });
 };
 
-export const getAllTickets = (token: any, companyId: any) => (dispatch: any) => {
-  dispatch(actions.startCall({ callType: callTypes.action }));
+export const getAllTickets =
+  (token: any, companyId: any) => (dispatch: any) => {
+    dispatch(actions.startCall({ callType: callTypes.action }));
+    return requestFromServer
+      .getAllTickets(token, companyId)
+      .then((response) => {
+        const { data } = response;
+        dispatch(actions.getedAllTicketsDetails({ data }));
+      })
+      .catch((error) => {
+        error.clientMessage = "Can't find patient test reports";
+        dispatch(actions.catchError({ error, callType: callTypes.action }));
+      });
+  };
+export const fetchRolePermissionsByUserId =
+  (id: any, token: any) => (dispatch: any) => {
+    dispatch(actions.startCall({ callType: callTypes.action }));
+    return requestFromServer
+      .getRolePermissionsById(id, token)
+      .then((response) => {
+        const { data } = response;
+        dispatch(actions.fetchedRolePermissionsById({ data }));
+      })
+      .catch((error) => {
+        error.clientMessage = "Can't find Role permissions By Id";
+        dispatch(actions.catchError({ error, callType: callTypes.action }));
+      });
+  };
+export const ChangeUser = (id:any, data: any, token: any) => (dispatch: any) => {
   return requestFromServer
-    .getAllTickets(token, companyId)
+    .ChangeUser(id,data, token)
     .then((response) => {
       const { data } = response;
-      dispatch(actions.getedAllTicketsDetails({ data }));
+      dispatch(actions.changedUser({ data }));
     })
     .catch((error) => {
-      error.clientMessage = "Can't find patient test reports";;
-      dispatch(actions.catchError({ error, callType: callTypes.action }));
+      // eslint-disable-next-line no-param-reassign
+      error.clientMessage = "Can't find";
+      dispatch(actions.catchError({ error, callType: callTypes.list }));
     });
-};
-export const fetchRolePermissionsByUserId = (id:any,token:any) => (dispatch:any) => {
-  dispatch(actions.startCall({ callType: callTypes.action }));
-  return requestFromServer
-    .getRolePermissionsById(id,token)
-    .then((response) => {
-      const { data } = response;
-      dispatch(actions.fetchedRolePermissionsById({ data }));
-    })
-    .catch((error) => {
-      error.clientMessage = "Can't find Role permissions By Id";;
-      dispatch(actions.catchError({ error, callType: callTypes.action }));
-    });
-};
+  };
+  export const getUsersById = (id: any, token: any) => (dispatch: any) => {
+    dispatch(actions.startCall({ callType: callTypes.action }));
+    return requestFromServer
+      .getUsers(id, token)
+      .then((response) => {
+        const { data } = response;
+        dispatch(actions.fetchedAllUsers({ data }));
+      })
+      .catch((error) => {
+        error.clientMessage = "Can't find patient test reports";
+        dispatch(actions.catchError({ error, callType: callTypes.action }));
+      });
+  };
--- src/app/pages/dashboard/_redux/dashboardCrud.ts
+++ src/app/pages/dashboard/_redux/dashboardCrud.ts
@@ -4,45 +4,58 @@ export const LEADS = "leads";
 export const USER = "contacts";
 export const DEALS = "deals";
 const campaigns = "campaigns";
+export const USERS = "users";
 export const TICKETS = "tickets";
 export const ROLE_PERMISSIONS = "rolepermissions";
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMGYwY2ViYmNhZmFkMTBkNjc2MjU5NiIsImlhdCI6MTY2NTQwMTE1OCwiZXhwIjoxNjY3OTkzMTU4fQ.F2z1tVzyk97WvI2Ee6cfqfyRiV8D4aO9UNoh7W_sVw0"
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMGYwY2ViYmNhZmFkMTBkNjc2MjU5NiIsImlhdCI6MTY2NTQwMTE1OCwiZXhwIjoxNjY3OTkzMTU4fQ.F2z1tVzyk97WvI2Ee6cfqfyRiV8D4aO9UNoh7W_sVw0";
 
 export function fetchAllCampaigns(token: any) {
   return axios.get(campaigns, {
-    headers: { "content-type": "application/json", Authorization: `Bearer ${token}` },
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
   });
 }
 export function fetchAllBookings() {
-
   return axios.get("http://43.205.49.41:5377/appointments", {
-    headers: { "content-type": "application/json", Authorization: `Bearer ${token}` },
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
   });
 }
 
 export function fetchAllTasks(token: any, companyId: any) {
-
-  return axios.get((`${TASKS}?company=${companyId}`), {
-    headers: { "content-type": "application/json", Authorization: `Bearer ${token}` },
+  return axios.get(`${TASKS}?company=${companyId}`, {
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
   });
 }
 
 export function fetchAllLeads(token: any, companyId: any) {
-
   return axios.get(`${LEADS}?company=${companyId}`, {
-    headers: { "content-type": "application/json", Authorization: `Bearer ${token}` },
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
   });
 }
 
-export function fetchAllContacts(token: any,companyId:any) {
-
+export function fetchAllContacts(token: any, companyId: any) {
   return axios.get(`${USER}?company=${companyId}`, {
-    headers: { "content-type": "application/json", Authorization: `Bearer ${token}` },
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
   });
 }
 
-export function fetchAllDeals(token: any,companyId:any) {
+export function fetchAllDeals(token: any, companyId: any) {
   return axios.get(`${DEALS}?company=${companyId}`, {
     headers: {
       "content-type": "application/json",
@@ -52,23 +65,41 @@ export function fetchAllDeals(token: any,companyId:any) {
 }
 
 export function getAllCdrs(token: any) {
-
   return axios.get(`cdrs`, {
-    headers: { "content-type": "application/json", Authorization: `Bearer ${token}` },
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
   });
 }
 
 export function getAllTickets(token: any, companyId: any) {
-
   return axios.get(`${TICKETS}?company=${companyId}`, {
-    headers: { "content-type": "application/json", Authorization: `Bearer ${token}` },
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
   });
 }
-export function getRolePermissionsById(id:any, token:any) {
+export function getRolePermissionsById(id: any, token: any) {
   return axios.get(`${ROLE_PERMISSIONS}?crmrole=${id}`, {
     headers: {
       "content-type": "application/json",
       Authorization: `Bearer ${token}`,
     },
+  });
+}
+export function ChangeUser(id: any, data: any, token: any) {
+  return axios.put(`${USERS}/${id}`, data, {
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
+export function getUsers(id: any, token: any) {
+
+  return axios.get(`${USERS}/${id?.id}`, {
+    headers: { "content-type": "application/json", Authorization: `Bearer ${token}` },
   });
 }
